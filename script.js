a9d2003c2956b37e42f19a6de194ba111328d489
Tina Armstead <tina.armstead@gmail.com>
2021-01-21 16:01:22 -0500

    Set up styling layout of the page

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 var APIKey = "1bf6cb587a3e66e892bb2c1caf691089";
 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Philadelphia,Pennsylvania&appid=" + APIKey;
 
-$.ajax({
+function currentWeather(city){
+  $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
       console.log(response);
       console.log(queryURL);
   });
+}
+
+function uvData(lat, lon){
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+      console.log(response);
+      console.log(queryURL);
+  });
+}
+
+function fiveDayForecast(city) {
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+      console.log(response);
+      console.log(queryURL);
+  });
+}
+
 
 //   add button with event listener
     // function that takes a user input
