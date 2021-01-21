c332667c6d330bb5ea02f3368e88e24800646a83
Tina Armstead <tina.armstead@gmail.com>
2021-01-21 16:34:33 -0500

    Constructed current weather api call and created a variable to capture the value of the input

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-var APIKey = "1bf6cb587a3e66e892bb2c1caf691089";
-
+let cityInput = $("#user-search").val();
+console.log(cityInput);
+let currentKey = "1bf6cb587a3e66e892bb2c1caf691089";
+let currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + currentKey + "&units=imperial"
 
 function currentWeather(city){
   $.ajax({
-    url: queryURL,
+    url: currentUrl,
     method: "GET"
   }).then(function(response) {
       console.log(response);
-      console.log(queryURL);
+      console.log(currentUrl);
   });
 }
+currentWeather();
+
+
 
 function uvData(lat, lon){
   $.ajax({
