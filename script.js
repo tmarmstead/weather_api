bda738dd575811ce96b8d1d8134b3f728b45a322
Tina Armstead <tina.armstead@gmail.com>
2021-01-22 21:54:33 -0500

    Test

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 let cityInput = $("#user-search").val();
-console.log(cityInput);
 let currentKey = "1bf6cb587a3e66e892bb2c1caf691089";
-let currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + currentKey + "&units=imperial"
+let currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + currentKey + "&units=imperial";
 
-function currentWeather(city){
+function currentWeather(cityInput){
   $.ajax({
     url: currentUrl,
     method: "GET"
   }).then(function(response) {
-      console.log(response);
+  
+    }
+      console.log($(response.main.temp));
       console.log(currentUrl);
   });
+  $("#clicked-button").on("click", (function() {
+    let cityInput = $("#user-search").val();
+    console.log(cityInput);
+    currentWeather();
+    let currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + currentKey + "&units=imperial"
+    console.log(currentUrl);
+    console.log($(response.main.temp));
+   
+    
+   
+  }))
 }
 currentWeather();
+
+
 
 
 
