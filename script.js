770b9a29b35c594c8716dc13e5d39dddcd80d04b
Tina Armstead <tina.armstead@gmail.com>
2021-01-27 16:48:28 -0500

    Re-structured ajax call and current weather functionality

--- script.js
+++ script.js
@@ -1,58 +1,61 @@
 let cityInput = $("#user-search").val();
-let currentKey = "1bf6cb587a3e66e892bb2c1caf691089";
+let currentKey = "eeee2e82be645c72cba0460f87b4f2c8";
 let currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + currentKey + "&units=imperial";
 
-function currentWeather(cityInput){
+// function currentWeather(){
+//   $.ajax({
+//     url: currentUrl,
+//     method : "GET"
+//   }).then(function(response) {
+//       console.log(response);
+//       console.log(currentUrl);
+//       console.log(cityInput);
+  
+
+$("button").click(function(event){
+  event.preventDefault();
+
+  let cityInput = $("#user-search").val();
+
+  let currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + currentKey + "&units=imperial";
+
   $.ajax({
     url: currentUrl,
-    method: "GET"
+    method : "GET"
   }).then(function(response) {
-  
-    }
-      console.log($(response.main.temp));
-      console.log(currentUrl);
-  });
-  $("#clicked-button").on("click", (function() {
-    let cityInput = $("#user-search").val();
-    console.log(cityInput);
-    currentWeather();
-    let currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + currentKey + "&units=imperial"
-    console.log(currentUrl);
-    console.log($(response.main.temp));
-   
-    
-   
-  }))
-}
-currentWeather();
-
-
-
-
-
-function uvData(lat, lon){
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
+      console.log('temperature is: ', response.main.temp);
+      console.log('entered city is: ', cityInput);
+      console.log('humidity is: ', response.main.humidity);
+      console.log('wind speed is: ', response.main.wind[speed]);
       console.log(response);
-      console.log(queryURL);
-  });
-}
-
-function fiveDayForecast(city) {
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-      console.log(response);
-      console.log(queryURL);
-  });
-}
+});
+});
 
 
+
+// function uvData(lat, lon){
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   }).then(function(response) {
+//       console.log(response);
+//       console.log(queryURL);
+//   });
+// }
+// uvData();
+
+// function fiveDayForecast(city) {
+//     $.ajax({
+//       url: queryURL,
+//       method: "GET"
+//     }).then(function(response) {
+//         console.log(response);
+//         console.log(queryURL);
+//     });
+//   }
+// fiveDayForecast();
 //   add button with event listener
     // function that takes a user input
     // click button to take in user input 
     // out put current weather info based of of user input
-    
+ 
