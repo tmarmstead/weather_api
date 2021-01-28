004eaa6f39ac81debf89998c0a444f975cc4ba76
Tina Armstead <tina.armstead@gmail.com>
2021-01-28 16:03:05 -0500

    Added path to UV information

--- script.js
+++ script.js
@@ -10,9 +10,11 @@ let currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInpu
 //       console.log(response);
 //       console.log(currentUrl);
 //       console.log(cityInput);
-  
 
-$("button").click(function(event){
+
+
+// Current Weather Functionality
+$("button").click(function (event) {
   event.preventDefault();
 
   let cityInput = $("#user-search").val();
@@ -21,15 +23,83 @@ $("button").click(function(event){
 
   $.ajax({
     url: currentUrl,
-    method : "GET"
-  }).then(function(response) {
-      console.log('temperature is: ', response.main.temp);
-      console.log('entered city is: ', cityInput);
-      console.log('humidity is: ', response.main.humidity);
-      console.log('wind speed is: ', response.main.wind[speed]);
-      console.log(response);
+    method: "GET"
+  }).then(function (response) {
+    $('.current-city-name').html(cityInput);
+    $('.current-temp').html('Temperature: ' + response.main.temp);
+    $('.current-humidity').html("<h3>" + 'Humidity: ' + response.main.humidity);
+    $('.current-wind').html('Wind: (fix this to display wind, not temp) ' + response.main.temp + 'mph');
+
+    console.log('temperature is: ', response.main.temp);
+    console.log('entered city is: ', cityInput);
+    console.log('humidity is: ', response.main.humidity);
+    console.log('wind speed is: ', response.main, response.main.wind);
+    console.log(response);
+
+
+    // UV functionality
+    let lat = response.coord.lat;
+    let lon = response.coord.lon;
+
+    let uvUrl = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + currentKey;
+
+    $.ajax({
+      url: uvUrl,
+      method: "GET"
+    }).then(function (response) {
+      console.log('Here is UV: ', response.value);
+
+      $('.current-uv').html('UV index: ' + response.value);
+
+      console.log(lat, lon);
+
+
+      // // 5 Day Forecast Functionality
+      
+      let forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=" + currentKey + "&units=imperial";
+
+      $.ajax({
+        url: forecastUrl,
+        method: "GET"
+      }).then(function (response) {
+        console.log('Here is 5 Day forecast: ', response, response.list);
+        
+          
+        
+        // for(i = 5; i <= 40; i + 8) {
+        
+        //  let fiveForecast = response.list
+        //  console.log(fiveForecast[i]);
+        // }
+        
+
+
+      });
+    });
+  });
 });
-});
+
+
+
+
+
+// $("button").click(function(event){
+//   event.preventDefault();
+
+//   let cityInput = $("#user-search").val();
+
+
+//   let currentUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=" + currentKey + "&units=imperial";
+
+//   $.ajax({
+//     url: currentUrl,
+//     method : "GET"
+//   }).then(function(response) {
+//     console.log('Here is 5 Day forecast');
+// });
+// });
+
+
 
 
 
@@ -58,4 +128,4 @@ $("button").click(function(event){
     // function that takes a user input
     // click button to take in user input 
     // out put current weather info based of of user input
- 
+
